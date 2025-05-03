339cf8405dd91385fd4ca37e6f9260b6c702af32
Kamil Surlas <kamsur2512@wp.pl>
2025-05-03 13:19:19 +0200

    Adjusted main and Makefile to work with new files.
    Json library have been added to the project.

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -8,11 +8,13 @@
 
 enum class Configuration
 {
-    USE_CONTAINER
+    USE_CONTAINER,
+    FROM_CMAKE
 }; 
 
 std::map<Configuration, bool> configMap = {
-    {Configuration::USE_CONTAINER, false}
+    {Configuration::USE_CONTAINER, false},
+    {Configuration::FROM_CMAKE, false}
 };
 
 struct ContainerParams
@@ -22,58 +24,60 @@ struct ContainerParams
 };
 
 ContainerParams configContainerParams(){
+    ContainerParams containerParams;
     printf("Enter the container name: ");
-    std::string containerName;
-    std::cin >> containerName;
+    std::cin >> containerParams.m_containerName;
     printf("Enter the container image: ");
-    std::string containerImage;
-    std::cin >> containerImage;
-    return {containerName, containerImage};
+    std::cin >> containerParams.m_containerImage;
+    return containerParams;
 }
-
-static const char* usage = "USAGE: StubsGenerator [fileName] [path to makefile] --use_container\n"
-                           "fileName: The name of the file to test.\n"
+static const char* const makefileName = "Makefile";
+static const char* const usage = "USAGE: StubsGenerator [path to makefile] [path to project directory] --use_container\n"
                            "path to makefile: The path to the makefile.\n"
-                           "--use_container: Add this flag if container should be used.\n";
+                           "path to project directory: The path to the project (needed for searching includes).\n"
+                           "--use_container: Add this flag if container should be used.\n"
+                           "--from_cmake: Use this flag if the Makefile have been generated from CMakeLists.txt\n";
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("%s", usage); 
+    if (argc < 2) {
+        printf("%s", usage);
         return 1;
     }
+
+    const std::string makefile = Utilities::FilesValidator::concatenate(argv[1], makefileName);
+    if (!Utilities::FilesValidator::validateIfFileExists(makefile) || 
+        !Utilities::FilesValidator::safetyCheck(makefile)) {
+        printf("Invalid path to makefile: %s\n", makefile.c_str());
+        return 1;
+    }
+
     for (int i = 1; i < argc; i++) {
         if (std::string(argv[i]) == "--use_container") {
             configMap[Configuration::USE_CONTAINER] = true;
         }
-    }
-    if (configMap[Configuration::USE_CONTAINER])
-    {
-        ContainerParams containerParams = configContainerParams();
-        try
-        {
-            ContainerCompilationExecutor executor(argv[1], argv[2], containerParams.m_containerName, containerParams.m_containerImage);
+        if (std::string(argv[i]) == "--from_cmake"){
+            configMap[Configuration::FROM_CMAKE] = true;
         }
-        catch(const std::exception& e)
-        {
+    }
+
+    if (configMap[Configuration::USE_CONTAINER]) {
+        ContainerParams containerParams = configContainerParams();
+        try {
+            ContainerCompilationExecutor executor(argv[1], containerParams.m_containerName, containerParams.m_containerImage);
+            CompilationOutputParser parser(executor.compile(), argv[1], configMap[Configuration::FROM_CMAKE] == true ? true : false);
+            parser.parse();
+        } catch (const std::exception& e) {
             std::cerr << e.what() << '\n';
         }
-    } else
-    {
-        Utilities::FilesValidator filesValidator(argv[1], argv[2]);
-        if (filesValidator.validate())
-        {
-            try
-            {
-                CompilationExecutor executor(argv[1], argv[2]);
-                CompilationOutputParser parser(executor.compile());
-                parser.parse();
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << '\n';
-            }
+    } else {
+        try {
+            CompilationExecutor executor(argv[1]);
+            CompilationOutputParser parser(executor.compile(), argv[1], configMap[Configuration::FROM_CMAKE] == true ? true : false);
+            parser.parse();
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << '\n';
         }
     }
-   
-    return 0;    
+
+    return 0;
 }
